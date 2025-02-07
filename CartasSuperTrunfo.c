9de4e54ea5b9751edbef9d0f6022d75b53ac061a
Wiillamss <willamsgomes95@gmail.com>
2025-02-07 13:16:00 +0000

    segunda edição

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,17 +14,22 @@ int main() {
 
     // Vai ser criado 2 países Brasil e Argentina
     // Dados abaixo se referente ao País Brasil
-    char estado = 'a';
-    char codigodacidade[01];
-    char cidade[5];
+    char estado /*São Paulo*/ = 'A';
+    char codigodacidade[5]; // Código que vai representar a cidade
+    char cidade[20]; // Cidade do Estado
     int populacao; // Quantidade total de pessoas
-    float area; // Area total em km² da cidade
-    float pib; // pib da cidade
-    int turisticos; // número de pontos turísticos
+    float area; // Área total em km² da cidade
+    float pib; // PIB da cidade
+    int turisticos; // Número de pontos turísticos
+    float densidadepopulacional; // População dividida pela área da cidade
+    float pibpercapita; // PIB total dividido pela população
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+    printf("Digite o código que representa a cidade: \n");
+    scanf("%s", &codigodacidade);
 
     printf("Digite o nome da cidade: \n");
     scanf("%s", &cidade);
@@ -41,17 +46,23 @@ int main() {
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &turisticos);
 
+    densidadepopulacional = (float) populacao / area;
+    pibpercapita = (int) pib / populacao;
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("A letra que representa o estado é: %c\n", estado);
+    printf("A letra que representa o estado de São Paulo é: %c\n", estado);
     printf("O código que representa a carta é: %s\n", codigodacidade);
     printf("O nome da cidade é: %s\n", cidade);
     printf("A quantidade de população é: %d\n", populacao);
-    printf("A área total é: %f - Area por Km²\n", area);
-    printf("O PIB que representa a cidade é: %.2f - Bilhões de reais\n", pib);
+    printf("A área total é: %.2f Km²\n", area);
+    printf("O PIB que representa a cidade é: %.2f Bilhões de reais\n", pib);
     printf("A quantidade de pontos turísticos é: %d\n", turisticos);
+    printf("A densidade populacional é: %.f pessoas por km²\n", pibpercapita);
+    printf("O PIB per capita por pessoa é: %d reais\n", densidadepopulacional);
 
     return 0;
 }
