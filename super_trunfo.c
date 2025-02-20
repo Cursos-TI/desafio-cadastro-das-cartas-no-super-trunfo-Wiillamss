49ad38a871631f1e452b15053ef5f815597aecbc
Wiillamss <willamsgomes95@gmail.com>
2025-02-20 11:32:02 +0000

    commint

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,18 +3,15 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Prática Willams Gomes
+// Prática Willams da Silva Gomes
 
 int main() {
     
-    printf(" *** Desafio Super Trunfo: Países ***\n");
+    printf("*** Desafio Super Trunfo: Países ***\n");
 
-    /* O objetivo é criar 8 estados, representados por letras
-    de A à H, e cada estado vai ter 4 cidades, acompanhando a letra do estado e um número,
-    exemplo: A01, A02, B01, B02. */
-
-    /*Cadastro das variaveis represetam de 1 a 4, exemplo: Estado1 = Variaveis de 1 à 2 e
-    Estado2 = Variaveis de 3 à 4*/
+    /*Cadastro das variaveis represetam de 1 a 4
+    As variaveis que representam o Estado1 são as variaveis 1 e 2
+    As variaveis que representam o Estado2 são as variaveis de 3 e 4*/
 
     char estado1, estado2; // Letra que representa o estado
     char codigodacidade1[5], codigodacidade2[5], codigodacidade3[5], codigodacidade4[5]; // Código que vai representar a cidade
@@ -27,18 +24,18 @@ int main() {
     float pibpercapita1, pibpercapita2, pibpercapita3, pibpercapita4; // PIB da cidade dividido pela população
 
 
-    // Entrada de Dados para a primeira carta
+    // Entrada de Dados para a primeira carta em Scanf
 
     printf(" *** Digite os dados da primeira carta *** \n");
 
-    printf("Digite a letra que representa o estado: \n");
+    printf("Digite uma letra de (A a H) que representa o estado: \n");
     scanf(" %c", &estado1);
 
     printf("Digite o código da cidade: \n");
-    scanf("%s", &codigodacidade1);
+    scanf("%s", codigodacidade1);
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &cidade1);
+    scanf("%s", cidade1);
 
     printf("Digite a quantidade total da população: \n");
     scanf("%d", &populacao1);
@@ -52,14 +49,14 @@ int main() {
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &turisticos1);
 
-    densidadepopulacional1 = (float) populacao1 / area1; // Atribuição a variavel densidadepopulacional
-    pibpercapita1 = (float) pib1 / populacao1; // Atribuição a variavel pibpercapita
+    densidadepopulacional1 = (float) populacao1 / area1; // Atribuição a variavel densidadepopulacional1
+    pibpercapita1 = (float) pib1 / populacao1; // Atribuição a variavel pibpercapita1
 
-// Comandos de exibição das Cartas
+// Comandos de exibição das Cartas usando Printf
     
     //Primeira Carta
 
-    printf(" *** Dados da primeira carta *** \n");
+    printf("Carta 1: \n");
     printf("A letra que representa o estado é: %c\n", estado1);   
     printf("O código que representa a carta é: %s\n", codigodacidade1);
     printf("O nome da cidade é: %s\n", cidade1);
@@ -70,18 +67,18 @@ int main() {
     printf("A densidade populacional é: %.2f de habitantes por km²\n", densidadepopulacional1);
     printf("O PIB per capita por pessoa é: %.2f reais\n", pibpercapita1);
 
-    // Entrada de Dados da segunda carta
+    // Entrada de Dados da segunda carta em Scanf
 
     printf(" *** Digite os dados da segunda carta *** \n");
 
-    printf("Digite a letra que representa o estado: \n");
+    printf("Digite uma letra de (A a H) que representa o estado: \n");
     scanf(" %c", &estado2);
 
     printf("Digite o código da cidade: \n");
-    scanf("%s", &codigodacidade3);
+    scanf("%s", codigodacidade3);
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &cidade3);
+    scanf("%s", cidade3);
 
     printf("Digite a quantidade total da população: \n");
     scanf("%d", &populacao3);
@@ -95,14 +92,14 @@ int main() {
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &turisticos3);
 
-    densidadepopulacional3 = (float) populacao3 / area3; // Atribuição a variavel densidadepopulacional
-    pibpercapita3 = (float) pib3 / populacao3; // Atribuição a variavel pibpercapita
+    densidadepopulacional3 = (float) populacao3 / area3; // Atribuição a variavel densidadepopulacional3
+    pibpercapita3 = (float) pib3 / populacao3; // Atribuição a variavel pibpercapita3
 
 // Comandos de exibição das Cartas
     
     // Segunda Carta
 
-    printf(" *** Dados da segunda carta *** \n");
+    printf("Carta 2:\n");
     printf("A letra que representa o estado é: %c\n", estado2);
     printf("O código que representa a carta é: %s\n", codigodacidade3);
     printf("O nome da cidade é: %s\n", cidade3);
